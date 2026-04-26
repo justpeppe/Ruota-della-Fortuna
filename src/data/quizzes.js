cb6c3b59590b02f9b000e24c22c218eb7a3e151f
justpeppe <trimarchig2002@gmail.com>
2026-04-26 20:59:29 +0200

    fix: copy

--- src/data/quizzes.js
+++ src/data/quizzes.js
@@ -1,7 +1,7 @@
 export const quizzes = [
   { id: 1, hint: "MAP", phrase: "SQUADRA SPECIALE CHE ESPLORA POSTI ABBANDONATI" },
   { id: 2, hint: "TENNIS", phrase: "SINNER TI FA UN BAFFO" },
-  { id: 3, hint: "GRANA PADANO", phrase: "UOMO SEPOLTO DAL SUO STESSO FORMAGGIO" },
+  { id: 3, hint: "FORME DI PARMIGIANO", phrase: "UOMO SEPOLTO DAL SUO STESSO FORMAGGIO" },
   { id: 4, hint: "BOSCO DI MALABOTTA", phrase: "LA PASSEGGIATA PIÙ LUNGA DEL PREVISTO" },
   { id: 5, hint: "INTONACO", phrase: "A MALTA È CADUTO AI TUOI PIEDI" },
   { id: 6, hint: "ORA LA PUOI DIRE", phrase: "LA DATA DEGLI ESAMI UNIVERSITARI" },
@@ -16,5 +16,5 @@ export const uiCopy = {
   ctaRules: "Leggi il Regolamento",
   ctaStart: "Inizia a Giocare",
   backButton: "⬅ Indietro",
-  rulesExplanation: "Il gioco si svolge su un tabellone composto da caselle che celano una frase misteriosa. Dott. Mastroeni Federico dovrai chiamare man mano le lettere e, se sono presenti nella frase, le caselle corrispondenti si illumineranno! Quando ti senti pronto, prova a dare la soluzione completa per vincere la manche. Buona fortuna, anche se dopo la laurea puoi superare qualsiasi sfida."
+  rulesExplanation: "Il gioco si svolge su un tabellone composto da caselle che celano una frase misteriosa. Dott. Mastroeni Federico dovrai chiamare man mano le lettere e, se sono presenti nella frase, le caselle corrispondenti si illumineranno! Per aiutarti con la tua ADHD potrai chiamare sia le consonanti che le vocali senza nessuna distinzione. Quando ti senti pronto, prova a dare la soluzione completa per vincere la manche. Buona fortuna, anche se dopo la laurea puoi superare qualsiasi sfida."
 };
