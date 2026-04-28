47557e0f9b1cf2a50ee0ea903a4e6434f99928e7
justpeppe <trimarchig2002@gmail.com>
2026-04-28 17:21:03 +0200

    FIX: copy

--- src/data/quizzes.js
+++ src/data/quizzes.js
@@ -1,10 +1,11 @@
 export const quizzes = [
-  { id: 1, hint: "MAP", phrase: "SQUADRA SPECIALE CHE ESPLORA POSTI ABBANDONATI" },
-  { id: 2, hint: "TENNIS", phrase: "SINNER TI FA UN BAFFO" },
-  { id: 3, hint: "FORME DI PARMIGIANO", phrase: "UOMO SEPOLTO DAL SUO STESSO FORMAGGIO" },
-  { id: 4, hint: "BOSCO DI MALABOTTA", phrase: "LA PASSEGGIATA PIÙ LUNGA DEL PREVISTO" },
-  { id: 5, hint: "INTONACO", phrase: "A MALTA È CADUTO AI TUOI PIEDI" },
-  { id: 6, hint: "ORA LA PUOI DIRE", phrase: "LA DATA DEGLI ESAMI UNIVERSITARI" },
+  { id: 1, hint: "LO DICI SEMPRE", phrase: "NON MI STRESSATE" },
+  { id: 2, hint: "MAP", phrase: "SQUADRA SPECIALE CHE ESPLORA POSTI ABBANDONATI" },
+  { id: 3, hint: "TENNIS", phrase: "SINNER TI FA UN BAFFO" },
+  { id: 4, hint: "FORME DI PARMIGIANO", phrase: "UOMO SEPOLTO DAL SUO STESSO FORMAGGIO" },
+  { id: 5, hint: "BOSCO DI MALABOTTA", phrase: "LA PASSEGGIATA PIÙ LUNGA DEL PREVISTO" },
+  { id: 6, hint: "INTONACO", phrase: "A MALTA È CADUTO AI TUOI PIEDI" },
+  { id: 7, hint: "ORA LA PUOI DIRE", phrase: "LA DATA DEGLI ESAMI UNIVERSITARI" },
 ];
 
 export const uiCopy = {
